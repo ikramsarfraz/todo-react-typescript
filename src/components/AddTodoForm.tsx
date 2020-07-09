99a2a81664208fd6005f491085fcc3a059d87d68
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-07-09 00:15:15 -0400

    Add Todo Items Component

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+interface AddTodoFormProps {
+  addTodo: AddTodo;
+}
+
+const AddTodoForm: React.FC<AddTodoFormProps> = ({ addTodo }) => {
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    addTodo(newTodo);
+    setNewTodo("");
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="New Todo ..."
+        value={newTodo}
+        onChange={handleChange}
+      />
+      <button type="submit" onClick={handleSubmit}>
+        +
+      </button>
+    </form>
+  );
+};
+
+export default AddTodoForm;
