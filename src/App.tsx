86a5a98a7c082f08664246bdac5111beda20fb13
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-07-09 00:17:59 -0400

    Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+// Styles
+import "./App.css";
+// Components
+import TodoList from "./components/TodoList";
+import AddTodoForm from "./components/AddTodoForm";
 
 const App: React.FC = () => {
-  return <div className="App">Todo App</div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const toggleTodo: ToggleTodo = (selectedTodo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = (newTodo) => {
+    newTodo.trim() !== "" &&
+      setTodos([...todos, { text: newTodo, complete: false }]);
+  };
+
+  return (
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
+    </>
+  );
 };
 
 export default App;
